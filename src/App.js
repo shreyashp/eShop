714699359a927ff4ec7291cdf15319ef7eddf207
Shreyash Pimpalshende <48014760+shreyashp@users.noreply.github.com>
2021-02-22 17:01:34 +0530

    App compenent with incomplete snackbar
    Udpated the app component and routing with material UI, also started a snackbar component render, ignore it currently as snackbar component is yet to be finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Provider } from 'react-redux';
 import Header from './components/header';
 import Home from './components/home';
 import Cart from './components/cart';
+import SimpleSnackbar from './components/snackbar'
 import data from './data'; //local fetching from data.js
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { Paper } from '@material-ui/core';
 
 function App() {
+
+  const theme = createMuiTheme({
+    palette: {
+    }
+  })
 
   const [users, setUser] = useState([]);
   useEffect(() => {
@@ -14,31 +23,39 @@ function App() {
   }, []);
 
   const loadUsers = async () => {
-    const result = await axios.get("http://localhost:8000/products");
+    const result = await axios.get("http://localhost:8000/store");
     setUser(result.data);
   }
   //console.log(users);
   //console.log(data);
 
   const products = users;
+
   const [cartItems, setcartItems] = useState([]);
+  const [open, setOpen] = useState('');
 
   const onAdd = (product) => {
     const exist = cartItems.find(x => x.id === product.id);
     if (exist) {
       setcartItems(cartItems.map(x => x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x));
+      setOpen('added')
     }
     else {
       setcartItems([...cartItems, { ...product, qty: 1 }]);
+      setOpen('added')
     }
+    console.log(open)
   }
 
   const onRemove = (product) => {
     const exist = cartItems.find((x) => x.id === product.id);
     if (exist.qty === 1) {
       setcartItems(cartItems.filter((x) => x.id !== product.id))
+      //open = "removed"
     } else {
       setcartItems(cartItems.map(x => x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x));
+     // open = "removed"
+
     }
   }
 
@@ -46,25 +63,30 @@ function App() {
     const exist = cartItems.find((x) => x.id === product.id);
     if (exist) {
       setcartItems(cartItems.filter((x) => x.id !== product.id))
+     // open = "removed";
     }
   };
 
   return (
-    <Router>
-      <div className="App">
-        <Header cartQuantity={cartItems.length}></Header>
+    <ThemeProvider theme={theme}>
+      <Paper style={{ height: '100vh' }}>
+        <Router>
+          <div className="App">
+            <Header cartQuantity={cartItems.length}></Header>
 
-        <Switch>
-          <Route path="/cart" component={Cart} >
-            <Cart onAdd={onAdd} onRemove={onRemove} onRemoveAll={onRemoveAll} cartItems={cartItems}></Cart>
-          </Route>
-          <Route path="/" exact component={Home}>
-            <Home onAdd={onAdd} products={products}></Home>
-          </Route>
-        </Switch>
-
-      </div>
-    </Router>
+            <Switch>
+              <Route path="/cart" component={Cart} >
+                <Cart onAdd={onAdd} onRemove={onRemove} onRemoveAll={onRemoveAll} cartItems={cartItems}></Cart>
+              </Route>
+              <Route path="/" exact component={Home}>
+                <Home onAdd={onAdd} products={products}></Home>
+              </Route>
+            </Switch>
+            <SimpleSnackbar open={open}></SimpleSnackbar>
+          </div>
+        </Router>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
