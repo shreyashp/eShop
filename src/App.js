25b82cc651f10542bdcfae14c1082a6169abb9f9
Shreyash Pimpalshende <48014760+shreyashp@users.noreply.github.com>
2021-02-24 14:01:16 +0530

    App component updated
    App component updated, removing the states and props used to pass data to child components and using redux therby for same purpose

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { Provider } from 'react-redux';
+import store from './redux/store'
 import Header from './components/header';
 import Home from './components/home';
 import Cart from './components/cart';
-import SimpleSnackbar from './components/snackbar'
-import data from './data'; //local fetching from data.js
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import { Paper } from '@material-ui/core';
@@ -17,75 +14,25 @@ function App() {
     }
   })
 
-  const [users, setUser] = useState([]);
-  useEffect(() => {
-    loadUsers();
-  }, []);
-
-  const loadUsers = async () => {
-    const result = await axios.get("http://localhost:8000/store");
-    setUser(result.data);
-  }
-  //console.log(users);
-  //console.log(data);
-
-  const products = users;
-
-  const [cartItems, setcartItems] = useState([]);
-  const [open, setOpen] = useState('');
-
-  const onAdd = (product) => {
-    const exist = cartItems.find(x => x.id === product.id);
-    if (exist) {
-      setcartItems(cartItems.map(x => x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x));
-      setOpen('added')
-    }
-    else {
-      setcartItems([...cartItems, { ...product, qty: 1 }]);
-      setOpen('added')
-    }
-    console.log(open)
-  }
-
-  const onRemove = (product) => {
-    const exist = cartItems.find((x) => x.id === product.id);
-    if (exist.qty === 1) {
-      setcartItems(cartItems.filter((x) => x.id !== product.id))
-      //open = "removed"
-    } else {
-      setcartItems(cartItems.map(x => x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x));
-     // open = "removed"
-
-    }
-  }
-
-  const onRemoveAll = (product) => {
-    const exist = cartItems.find((x) => x.id === product.id);
-    if (exist) {
-      setcartItems(cartItems.filter((x) => x.id !== product.id))
-     // open = "removed";
-    }
-  };
-
   return (
     <ThemeProvider theme={theme}>
-      <Paper style={{ height: '100vh' }}>
-        <Router>
-          <div className="App">
-            <Header cartQuantity={cartItems.length}></Header>
-
-            <Switch>
-              <Route path="/cart" component={Cart} >
-                <Cart onAdd={onAdd} onRemove={onRemove} onRemoveAll={onRemoveAll} cartItems={cartItems}></Cart>
-              </Route>
-              <Route path="/" exact component={Home}>
-                <Home onAdd={onAdd} products={products}></Home>
-              </Route>
-            </Switch>
-            <SimpleSnackbar open={open}></SimpleSnackbar>
-          </div>
-        </Router>
-      </Paper>
+      <Provider store={store}>
+        <Paper style={{ height: '100vh' }}>
+          <Router>
+            <div className="App">
+              <Header ></Header>
+              <Switch>
+                <Route path="/cart" component={Cart} >
+                  <Cart></Cart>
+                </Route>
+                <Route path="/" exact component={Home}>
+                  <Home ></Home>
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        </Paper>
+      </Provider>
     </ThemeProvider>
   );
 }
